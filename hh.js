87f107188e8a8f71474467b75fa07d8a74fcc28c
Saidumarov <saidumarovjafarxon@gmail.com>
2023-04-19 21:12:20 +0500

    commit

---
+++ hh.js
@@ -0,0 +1,19 @@
+const menyu = document.querySelector(".ul-men")
+const menyu1 = document.querySelector("menyu")
+const menyu2 = document.querySelector("menyu2")
+
+function ok() {
+	
+	document.querySelector("ul").style.height = ("180px");
+	document.querySelector(".menyu").style.opacity = ("-1")
+	document.querySelector(".menyu2").style.opacity = ("1")
+	document.querySelector("li").style.opacity = ("1")
+}
+
+function ok1() {
+	document.querySelector(".ul-men").style.height = ("0px");
+	document.querySelector(".menyu").style.opacity = ("1")
+	document.querySelector(".menyu2").style.opacity = ("-1")
+	document.querySelector("li").style.opacity = ("-1")
+}
+
